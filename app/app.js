d158a9ff9a47a1789b494fbd46f860ab31c7ae5d
supersuryaansh <prod.genx@gmail.com>
2024-07-25 10:17:01 +0530

    Switched to type module

--- app/app.js
+++ app/app.js
@@ -1,20 +1,23 @@
 /* global Bare, BareKit */
 
 Bare
-  .on('suspend', () => console.log('suspended'))
-  .on('resume', () => console.log('resumed'))
-  .on('exit', () => console.log('exited'))
+    .on('suspend', () => console.log('suspended'))
+    .on('resume', () => console.log('resumed'))
+    .on('exit', () => console.log('exited'))
 
-const Hyperswarm = require('hyperswarm')
-const Hypercore = require('hypercore')
-var fs = require('bare-fs');
+import Hyperswarm from "hyperswarm";
+import Hypercore from "hypercore";
+import fs from "bare-fs";
 
 // We need to store hypercore in /tmp/foldername
 var dir = './tmp/hyperclip/';
 // Check if the directory exists, delete and recreate if it already exists. Prevents issue while working with multiple hypercore
 if (fs.existsSync(dir)) {
     // Remove the directory and its contents
-    fs.rmSync(dir, { recursive: true, force: true });
+    fs.rmSync(dir, {
+        recursive: true,
+        force: true
+    });
 }
 
 // Create the directory
@@ -28,36 +31,36 @@ const core = new Hypercore('./tmp/hyperclip/', "HYPERCLIP_DESKTOP_KEY")
 
 // Create RPC
 const rpc = new BareKit.RPC((req) => {
-  //can establish two-way communication here later
+    //can establish two-way communication here later
 })
 
-async function main() {
+await core.ready()
+const foundPeers = core.findingPeers()
+swarm.join(core.discoveryKey)
+swarm.on('connection', conn => core.replicate(conn))
+// swarm.flush() will wait until *all* discoverable peers have been connected to
+// It might take a while, so don't await it
+// Instead, use core.findingPeers() to mark when the discovery process is completed
+swarm.flush().then(() => foundPeers())
 
-    await core.ready()
-    const foundPeers = core.findingPeers()
-    swarm.join(core.discoveryKey)
-    swarm.on('connection', conn => core.replicate(conn))
-    // swarm.flush() will wait until *all* discoverable peers have been connected to
-    // It might take a while, so don't await it
-    // Instead, use core.findingPeers() to mark when the discovery process is completed
-    swarm.flush().then(() => foundPeers())
+// This won't resolve until either
+//    a) the first peer is found
+// or b) no peers could be found
+await core.update()
+let position = core.length
+console.log(`Skipping ${core.length} earlier blocks...`)
 
-    // This won't resolve until either
-    //    a) the first peer is found
-    // or b) no peers could be found
-    await core.update()
-    let position = core.length
-    console.log(`Skipping ${core.length} earlier blocks...`)
-
-    // Skip earlier block so that we have the latest clipboard data
-    for await (const block of core.createReadStream({start: 0, live: true})) {
-        // Send a RPC 'ping' signal to AppDelegate.m
-        let req = rpc.request('ping')
-        // Send data along with the ping. This contains clipboard content received from the desktop app.
-        req.send(block)
-        console.log(`Block ${position++}: ${block}`)
-    }
-
+// Skip earlier block so that we have the latest clipboard data
+for await (const block of core.createReadStream({
+    start: 0,
+    live: true
+})) {
+    // Send a RPC 'ping' signal to AppDelegate.m
+    let req = rpc.request('ping')
+    // Send data along with the ping. This contains clipboard content received from the desktop app.
+    req.send(block)
+    console.log(`Block ${position++}: ${block}`)
 }
 
-main();
+
+
